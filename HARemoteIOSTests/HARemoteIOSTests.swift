4d751ef3008c2bca41390cf030bf5ac85eab5706
Markus Bach <mb>
2025-11-21 15:05:55 +0100

    Initial Commit

---
+++ HARemoteIOSTests/HARemoteIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  HARemoteIOSTests.swift
+//  HARemoteIOSTests
+//
+//  Created by Markus Bach on 21.11.25.
+//
+
+import Testing
+@testable import HARemoteIOS
+
+struct HARemoteIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
