d47ef72597b03a3fb0fa92de47820e74bd6dede9
Markus Bach <mb@ehrke-bach.de>
2025-11-27 15:13:24 +0100

    progress views

---
+++ HARemoteIOS/TemplateViews/BackgroundImage.swift
@@ -0,0 +1,18 @@
+//
+//  BackgroundImage.swift
+//  HARemoteIOS
+//
+//  Created by Markus Bach on 27.11.25.
+//
+
+import SwiftUI
+
+struct BackgroundImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackgroundImage()
+}
