eda107b044ad69fdac9aeb5fd9de236c3e3f25eb
Markus Bach <mb@ehrke-bach.de>
2025-11-27 16:02:43 +0100

    some grid fun

---
+++ HARemoteIOS/TemplateViews/Grid3x4.swift
@@ -0,0 +1,18 @@
+//
+//  Grid3x4.swift
+//  HARemoteIOS
+//
+//  Created by Markus Bach on 27.11.25.
+//
+
+import SwiftUI
+
+struct Grid3x4: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Grid3x4()
+}
