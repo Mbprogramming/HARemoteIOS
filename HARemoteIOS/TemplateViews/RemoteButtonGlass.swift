f5b1b1742328adad9c2d8be7da36a76336db10fa
Markus Bach <mb@ehrke-bach.de>
2025-12-01 16:57:45 +0100

    more styling

---
+++ HARemoteIOS/TemplateViews/RemoteButtonGlass.swift
@@ -0,0 +1,18 @@
+//
+//  RemoteButtonGlass.swift
+//  HARemoteIOS
+//
+//  Created by Markus Bach on 01.12.25.
+//
+
+import SwiftUI
+
+struct RemoteButtonGlass: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RemoteButtonGlass()
+}
