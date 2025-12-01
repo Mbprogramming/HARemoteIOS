5a91e3918a87bb1e85b2e04529a82dab9421bc3e
Markus Bach <mb@ehrke-bach.de>
2025-12-01 16:44:10 +0100

    main commands begin

---
+++ HARemoteIOS/MainCommandsView.swift
@@ -0,0 +1,18 @@
+//
+//  MainCommandsView.swift
+//  HARemoteIOS
+//
+//  Created by Markus Bach on 01.12.25.
+//
+
+import SwiftUI
+
+struct MainCommandsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainCommandsView()
+}
